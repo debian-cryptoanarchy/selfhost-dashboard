23f2ff25f5054affbc450856eddc3eab145ccd36
Martin Habovstiak <martin.habovstiak@gmail.com>
2021-01-18 12:11:13 +0100

    Don't hard-code the path

--- static/dashboard.js
+++ static/dashboard.js
@@ -15,7 +15,7 @@ xhr.onreadystatechange = function() {
         var apps_html = ""
         for (app of response.apps) {
             console.log(app);
-            apps_html += "<div class=\"col-sm-2\"><div class=\"card fort-tile\"><a href=\""+ app.url +"\" target=\"_blank\"><img src=\"/dashboard"+ app.icon +"\" class=\"fort-tileImg float-left\" alt=\"\"></a><h5 class=\"card-title\"><a class=\"fort-tileName text-break text-center\"  target=\"_blank\" href=\""+ app.url +"\">"+app.name+"</a></h5></div></div>"
+            apps_html += "<div class=\"col-sm-2\"><div class=\"card fort-tile\"><a href=\""+ app.url +"\" target=\"_blank\"><img src=\"" + root_path + app.icon +"\" class=\"fort-tileImg float-left\" alt=\"\"></a><h5 class=\"card-title\"><a class=\"fort-tileName text-break text-center\"  target=\"_blank\" href=\""+ app.url +"\">"+app.name+"</a></h5></div></div>"
         }
         document.getElementById("apps").innerHTML = apps_html;
     }
